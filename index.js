d5f0826885bd696b425f9581b77f3e5cebd2f16b
Liliana993 <113596545+Liliana993@users.noreply.github.com>
2024-01-18 13:57:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const elLista = document.querySelector('.list');
+  elLista.replaceChildren();
+
+  cosasAprendidas.forEach((elem) => {
+    const nuevoElLi = document.createElement(´li´);
+    nueevoElLi.innerText = elem.tema;
+    if (elem.class.lenght > 0)
+      nuevoElLi.classList.add(elem.class);
+    elLista.appens(nuevoElLi);
+});
 
 main();
